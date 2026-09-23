6fde64ee6667dd20ab659047f2c0c9b2af5538d2
AlphaHR <ershadaislam@gmail.com>
2026-09-23 15:27:56 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3RJ7-Czo03NVpoLB_H4_kE4y2UANnV8",
+  authDomain: "nice-cosmic.firebaseapp.com",
+  databaseURL: "https://nice-cosmic-default-rtdb.firebaseio.com",
+  projectId: "nice-cosmic",
+  storageBucket: "nice-cosmic.firebasestorage.app",
+  messagingSenderId: "259173761472",
+  appId: "1:259173761472:web:6125f02c14e5e919730825",
+  measurementId: "G-G9YP2GV1BY"
+};
+
+firebase.initializeApp(firebaseConfig);
